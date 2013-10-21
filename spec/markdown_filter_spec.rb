982d1c454c5287da9d0536dd2676ae40e231f93b
Scott Windsor <swindsor@gmail.com>
2013-10-21 00:29:42 -0700

    Update spec for latest markdown

--- spec/markdown_filter_spec.rb
+++ spec/markdown_filter_spec.rb
@@ -7,16 +7,20 @@ describe MarkdownFilter do
 <!DOCTYPE html PUBLIC
     \"-//W3C//DTD XHTML 1.1 plus MathML 2.0 plus SVG 1.1//EN\"
     \"http://www.w3.org/2002/04/xhtml-math-svg/xhtml-math-svg.dtd\">
-<html xml:lang='en' xmlns:svg='http://www.w3.org/2000/svg' xmlns='http://www.w3.org/1999/xhtml'>
-<head><meta content='application/xhtml+xml;charset=utf-8' http-equiv='Content-type' /><title>Foo</title></head>
+<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:svg=\"http://www.w3.org/2000/svg\" xml:lang=\"en\">
+<head><meta http-equiv=\"Content-type\" content=\"application/xhtml+xml;charset=utf-8\" /><title>Foo</title></head>
 <body>
-<h1 id='foo'>Foo</h1>
+<h1 id="foo">Foo</h1>
 </body></html>
 eos
 ).chomp
   end
 
   it "should filter markdown partial" do
-    MarkdownFilter.new.call("# Foo", :partial => true).must_equal "<h1 id='foo'>Foo</h1>"
+    MarkdownFilter.new.call("# Foo", :partial => true).must_equal (<<-eos
+
+<h1 id="foo">Foo</h1>
+eos
+)
   end
 end
